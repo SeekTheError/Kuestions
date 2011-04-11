d49818ac0f52349ae6a1728e47c925d124817ab4
SeekTheError <remi.bouchar@gmail.com>
2011-04-12 05:27:28 +0900

    add user authentication to the controller

--- kuestionsWS/controller/indexcontroller.py
+++ kuestionsWS/controller/indexcontroller.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+import security
 
 def view(request) :
   t = loader.get_template('index.html')
-  c=RequestContext(request)
-  return HttpResponse(t.render(c))
+  context=RequestContext(request)
+  context=security.addUserInfoToContext(request,context)
+  return HttpResponse(t.render(context))
 
 
