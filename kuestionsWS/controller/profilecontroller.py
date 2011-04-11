6bb929189f1554fec57cd513a5a5a3f8b06ee010
SeekTheError <remi.bouchar@gmail.com>
2011-04-12 07:22:08 +0900

    add a HttpRedirect

--- kuestionsWS/controller/profilecontroller.py
+++ kuestionsWS/controller/profilecontroller.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import Context,RequestContext, loader
 
 from couchdbinterface.couchdblayer import *
@@ -6,12 +6,12 @@ import  couchdbinterface.couchdblayer
 import security
 
 def current(request) :
-  print 'current'
   context = RequestContext(request)
   context=security.addUserInfoToContext(request,context)
   t = loader.get_template('profile.html')
   if context['sessionIsOpen']== True :
-    return HttpResponse(t.render(context))
+    login=context['user'].login
+    return HttpResponseRedirect('/kuestions/user/'+login)
   else : 
     return HttpResponse(t.render(context))
   
@@ -20,24 +20,18 @@ def current(request) :
 def view(request,login) :
   #TODO distinguish those case: the user see his page, the user see another page
   #And : Distinguish GET/POST method (need to split in different method)
-  
   context = RequestContext(request)
   context = security.addUserInfoToContext(request,context)
   currentUser=security.getCurrentUser(context)
   if currentUser and currentUser.login == login:
     print currentUser.login
-    context['isAdmin']=True
+    context['isAdmin'] = True
   else :
     user=User(login=login)
     user=user.findByLogin()
     context["user"]=user
-  
   t = loader.get_template('profile.html')
-  if user != None :    
-    return HttpResponse(t.render(context))
-  #TODO code the user not found aspect
-  else :
-    return HttpResponse(t.render(context))
+  return HttpResponse(t.render(context))
     
     
     
