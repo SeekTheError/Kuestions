5c8edfbb66f2de3a88a381d9c8341d193e6e631b
SeekTheError <remi.bouchar@gmail.com>
2011-05-01 04:27:05 +0900

    javascript mod

--- static/js/kuestions.js
+++ static/js/kuestions.js
@@ -1,6 +1,20 @@
+window.onload = function() {
+	init();
+}
+
+var lastSearchEtag='';
+
+function init() {
+	//google instant style
+	$('#searchBar').keyup(function(event) {
+		 if (event.which > 47 | event.which==8)
+			searchQuestion();
+	});
+}
+
 function searchQuestion() {
 
-	var searchParameter = document.getElementById("searchParameter").value;
+	var searchParameter = document.getElementById("searchBar").value;
 	var url = '/api/_fti/_design/question/by_content?q=' + searchParameter;
 	$.ajax({
 		url : url,
@@ -12,6 +26,7 @@ function searchQuestion() {
 }
 
 function displaySearchResults(data) {
+	
 	object = eval(data);
 	rows = object.rows;
 	if (object.rows) {
@@ -26,18 +41,20 @@ function displaySearchResults(data) {
 		rows = object.rows;
 		length = rows.length;
 		for (i = 0; i < length; i++) {
-			question = rows[i].fields;
-			question.id = rows[i].id;
-			li = document.createElement("li");
-			li.appendChild(formatQuestion(question));
-			ul.appendChild(li);
+			if (rows[i].score >= 0.8) {
+				question = rows[i].fields;
+				question.id = rows[i].id;
+				li = document.createElement("li");
+				li.appendChild(formatQuestion(question));
+				ul.appendChild(li);
+			}
 		}
 	}
 }
 function formatQuestion(question) {
 	span = document.createElement("span");
 	p = document.createElement("p");
-	p.textContent = question.default;
+	p.textContent = question.content;
 	span.appendChild(p);
 	return p;
 }
