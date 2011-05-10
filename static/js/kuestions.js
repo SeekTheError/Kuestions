8ef3287a7a239721503d2539c4f916d62f59c5d7
Jihoon Baek <hoonji@hotmail.com>
2011-05-10 19:50:11 +0900

    fixed refresh followed question list when follow/unfollow button pressed

--- static/js/kuestions.js
+++ static/js/kuestions.js
@@ -273,7 +273,7 @@ function manageFollowQuestion(){
 
 
       //if followed tab is selected
-      if ( $('#followedTab li').attr('className') == 'selected'){
+      if ( $('#followedTab li').attr('className').indexOf('selected') != -1 ){
         //refresh followed list in question view
         displayFollowedQuestions();
       }
