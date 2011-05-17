95946e1c0a256243b9ceb0da5c27eb9c1570dbfd
SeekTheError <remi.bouchar@gmail.com>
2011-05-18 06:54:32 +0900

    c

--- static/js/kuestions.js
+++ static/js/kuestions.js
@@ -432,7 +432,7 @@ function checkForNewAnswerDaemon(questionId){
 		    		
 		    	}
 		    }});	
-	setTimeout("checkForNewAnswerDaemon(questionId,answerCount)",2000);
+	setTimeout("checkForNewAnswerDaemon(questionId)",2000);
 	}
 	console.log("no match!");
 	
