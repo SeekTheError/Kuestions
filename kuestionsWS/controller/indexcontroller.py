e5e4090da9117d75113ea0f580c9d420bcd4a6e3
SeekTheError <remi.bouchar@gmail.com>
2011-05-19 22:30:51 +0900

    replace the temp topics list with the actual topics of the database

--- kuestionsWS/controller/indexcontroller.py
+++ kuestionsWS/controller/indexcontroller.py
@@ -2,13 +2,18 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from security.userauth import checkSession
+from couchdbinterface import dblayer
 
 def view(request) :
   t = loader.get_template('index.html')
   context=RequestContext(request)
   context=checkSession(request,context)
-  
-  context['topics']=['Java', 'Hello', 'Hi', 'answer', 'kaist']
+  duplicatedTopics=dblayer.view('topics/topic')
+  topics=[]
+  for row in duplicatedTopics.view().rows:
+    if topics.__contains__(row.key)==False:
+      topics.append(row.key)
+  context['topics']=topics
   
   return HttpResponse(t.render(context))
 
